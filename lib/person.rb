42802f9984001c3edc6d2c0f5e7cb0fba460bf49
Michael David Sackstein <m_sackstein@yahoo.com>
2020-10-06 01:52:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,15 @@
 class Person
   attr_accessor :name, :job
 
+  @@all = []
+
   def initialize(name, job)
     @name = name
     @job = job
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
