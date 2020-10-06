931438cb245ee4cd51c8190b58af7611ba1361c2
Michael David Sackstein <m_sackstein@yahoo.com>
2020-10-06 01:52:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
 class Dog
   attr_accessor :name, :breed
 
+  @@all = []
+
   def initialize(name, breed)
     @name = name
     @breed = breed
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 end
